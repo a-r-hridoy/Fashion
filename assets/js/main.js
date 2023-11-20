1dc1086b50cae95e1fdbac44e3d64f4a0e6b3898
unknown <arhridoy@gmail.com>
2023-11-20 17:03:36 +0600

    change js

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,30 +45,30 @@ $(".stop").on("click", function () {
 
 // virtual-start
 
-document.addEventListener("DOMContentLoaded", function () {
-   const list = ["glasses1", "glasses2", "hat1", "hat2", "earring"];
-   const visibles = [true, false, false, true, true];
-   const setVisible = (button, entities, visible) => {
-      if (visible) {
-         button.classList.add("selected");
-      } else {
-         button.classList.remove("selected");
-      }
-      entities.forEach((entity) => {
-         entity.setAttribute("visible", visible);
-      });
-   };
-   list.forEach((item, index) => {
-      const button = document.querySelector("#" + item);
-      const entities = document.querySelectorAll("." + item + "-entity");
-      setVisible(button, entities, visibles[index]);
-      button.addEventListener("click", () => {
-         visibles[index] = !visibles[index];
-         setVisible(button, entities, visibles[index]);
-      });
-   });
+// document.addEventListener("DOMContentLoaded", function () {
+//    const list = ["glasses1", "glasses2", "hat1", "hat2", "earring"];
+//    const visibles = [true, false, false, true, true];
+//    const setVisible = (button, entities, visible) => {
+//       if (visible) {
+//          button.classList.add("selected");
+//       } else {
+//          button.classList.remove("selected");
+//       }
+//       entities.forEach((entity) => {
+//          entity.setAttribute("visible", visible);
+//       });
+//    };
+//    list.forEach((item, index) => {
+//       const button = document.querySelector("#" + item);
+//       const entities = document.querySelectorAll("." + item + "-entity");
+//       setVisible(button, entities, visibles[index]);
+//       button.addEventListener("click", () => {
+//          visibles[index] = !visibles[index];
+//          setVisible(button, entities, visibles[index]);
+//       });
+//    });
    
-});
+// });
 
 
 // t-categoriesCarousel
