fb2734be53c8e44f9ee6a48778159c6b99d24304
unknown <arhridoy@gmail.com>
2023-11-20 16:41:02 +0600

    virtual js change

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,6 +42,35 @@ $(".stop").on("click", function () {
    owl.trigger("stop.owl.autoplay");
 });
 
+
+// virtual-start
+
+document.addEventListener("DOMContentLoaded", function () {
+   const list = ["glasses1", "glasses2", "hat1", "hat2", "earring"];
+   const visibles = [true, false, false, true, true];
+   const setVisible = (button, entities, visible) => {
+      if (visible) {
+         button.classList.add("selected");
+      } else {
+         button.classList.remove("selected");
+      }
+      entities.forEach((entity) => {
+         entity.setAttribute("visible", visible);
+      });
+   };
+   list.forEach((item, index) => {
+      const button = document.querySelector("#" + item);
+      const entities = document.querySelectorAll("." + item + "-entity");
+      setVisible(button, entities, visibles[index]);
+      button.addEventListener("click", () => {
+         visibles[index] = !visibles[index];
+         setVisible(button, entities, visibles[index]);
+      });
+   });
+   
+});
+
+
 // t-categoriesCarousel
 
 var owl = $(".t-categories-carousel");
@@ -271,30 +300,4 @@ function spin() {
 }
 
 // Discount wheel end
-
-// virtual-start
-
-document.addEventListener("DOMContentLoaded", function () {
-   const list = ["glasses1", "glasses2", "hat1", "hat2", "earring"];
-   const visibles = [true, false, false, true, true];
-   const setVisible = (button, entities, visible) => {
-      if (visible) {
-         button.classList.add("selected");
-      } else {
-         button.classList.remove("selected");
-      }
-      entities.forEach((entity) => {
-         entity.setAttribute("visible", visible);
-      });
-   };
-   list.forEach((item, index) => {
-      const button = document.querySelector("#" + item);
-      const entities = document.querySelectorAll("." + item + "-entity");
-      setVisible(button, entities, visibles[index]);
-      button.addEventListener("click", () => {
-         visibles[index] = !visibles[index];
-         setVisible(button, entities, visibles[index]);
-      });
-   });
-});
 
